78b3f365e239abc5ce2374dc57b76defc1589b51
NataliaNavarro1 <123481210+NataliaNavarroRea@users.noreply.github.com>
2023-03-18 09:35:25 -0600

    Reto6

--- src/edu/natalia/navarro/reto6/Process/AdivinaLaPalabra.java
+++ src/edu/natalia/navarro/reto6/Process/AdivinaLaPalabra.java
@@ -1,5 +1,6 @@
 package edu.natalia.navarro.reto6.Process;
 
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -7,90 +8,114 @@ import java.util.Scanner;
  * Presenta un proceso de adivinar las palabras, contiene la lógica y los datos del juego
  * Representa un juego para adivinar una palabra.
  */
-public class AdivinaLaPalabra {
-        private static final String[] PALABRAS_FACILES = {"casa", "mesa", "perro", "sol", "agua", "auto", "mano"};
-        private static final String[] PALABRAS_INTERMEDIAS = {"computadora", "perfeccionista", "baloncesto", "jardineria", "historiador", "noticiero"};
-        private static final String[] PALABRAS_DIFICILES = {"extraterrestre", "arqueologia", "serendipia", "espectrometro", "hipopotomonstrosesquipedaliofobia"};
 
-        private String[] palabrasDisponibles;
-        private String palabraSeleccionada;
-        private char[] letrasAdivinadas;
+public class AdivinaLaPalabra {
+    private static final String[] PALABRAS_FACILES = {"casa", "mesa", "perro", "sol", "agua", "auto", "mano"};
+    private static final String[] PALABRAS_INTERMEDIAS = {"computadora", "perfeccionista", "baloncesto", "jardineria", "historiador", "noticiero"};
+    private static final String[] PALABRAS_DIFICILES = {"extraterrestre", "arqueologia", "serendipia", "espectrometro", "hipopotomonstrosesquipedaliofobia"};
+
+    private String[] palabrasDisponibles;
+    private String palabraSeleccionada;
+    private char[] letrasAdivinadas;
 
     /**
-     * Constructor de la clase JuegoAdivinaPalabra
+     * Constructor de la clase AdivinaLaPalabra
+     *
      * @param nivel Nivel del juego (1 para fácil, 2 para intermedio, 3 para avanzado)
      */
-        public AdivinaLaPalabra(int nivel) {
-            switch (nivel) {
-                case 1:
-                    palabrasDisponibles = PALABRAS_FACILES;
-                    break;
-                case 2:
-                    palabrasDisponibles = PALABRAS_INTERMEDIAS;
-                    break;
-                case 3:
-                    palabrasDisponibles = PALABRAS_DIFICILES;
-                    break;
-                default:
-                    System.out.println("Opción inválida");
-                    break;
-            }
+    public AdivinaLaPalabra(int nivel) throws IllegalArgumentException {
+        switch (nivel) {
+            case 1:
+                palabrasDisponibles = PALABRAS_FACILES;
+                break;
+            case 2:
+                palabrasDisponibles = PALABRAS_INTERMEDIAS;
+                break;
+            case 3:
+                palabrasDisponibles = PALABRAS_DIFICILES;
+                break;
+            case 0:
+                palabrasDisponibles = new String[]{};
+                break;
+            default:
+                throw new IllegalArgumentException("Opción inválida");
         }
+    }
+
     /**
-     * En este metodo es el inicio del juego
+     * En este método es el inicio del juego
      */
+    public void jugar() {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        boolean jugando = true;
 
-        public void jugar() {
-            Scanner scanner = new Scanner(System.in);
-            Random random = new Random();
-            boolean jugando = true;
+        while (jugando) {
+            int indicePalabra = random.nextInt(palabrasDisponibles.length);
+            palabraSeleccionada = palabrasDisponibles[indicePalabra];
+            letrasAdivinadas = new char[palabraSeleccionada.length()];
+            for (int i = 0; i < letrasAdivinadas.length; i++) {
+                letrasAdivinadas[i] = '_';
+            }
 
-            while (jugando) {
-                int indicePalabra = random.nextInt(palabrasDisponibles.length);
-                palabraSeleccionada = palabrasDisponibles[indicePalabra];
-                letrasAdivinadas = new char[palabraSeleccionada.length()];
-                for (int i = 0; i < letrasAdivinadas.length; i++) {
-                    letrasAdivinadas[i] = '_';
-                }
+            boolean adivinada = false;
+            int intentos = 0;
+            while (!adivinada) {
+                System.out.println("La palabra a adivinar es:");
+                System.out.println(letrasAdivinadas);
+                System.out.println("Ingresa una letra:");
 
-                boolean adivinada = false;
-                int intentos = 0;
-                while (!adivinada) {
-                    System.out.println("La palabra a adivinar es:");
-                    System.out.println(letrasAdivinadas);
-                    System.out.println("Ingresa una letra:");
+                try {
                     String entrada = scanner.next();
-                    char letra = entrada.charAt(0);
-                    if (letra == '0') {
+                    if (entrada.equals("0")) {
                         jugando = false;
                         break;
-
                     }
+                    char letra = entrada.charAt(0);
                     if (!Character.isLetter(letra)) {
-                        System.out.println("Ingresa un carácter válido");
+                        System.out.println("Ingresa una letra válida");
                         continue;
-
                     }
                     boolean encontrada = false;
                     for (int i = 0; i < palabraSeleccionada.length(); i++) {
                         if (Character.toLowerCase(letra) == Character.toLowerCase(palabraSeleccionada.charAt(i))) {
                             encontrada = true;
                             letrasAdivinadas[i] = letra;
-
                         }
                     }
-
                     if (!encontrada) {
                         intentos++;
                         System.out.println("¡Intenta de nuevo!");
+
                     }
-                    if (palabraSeleccionada.equals(String.valueOf(letrasAdivinadas))) {
-                        adivinada = true;
-                        System.out.println("¡Felicidades, adivinaste la palabra!");
+                    if (intentos == 6) {
+                        System.out.println("Lo siento, perdiste. La palabra era: " + palabraSeleccionada);
+                        break;
                     }
+                    if (String.valueOf(letrasAdivinadas).equals(palabraSeleccionada)) {
+                        System.out.println("¡Felicitaciones! Adivinaste la palabra: " + palabraSeleccionada);
+                        break;
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("Ingresa una letra válida");
+                    scanner.next();
                 }
             }
-            scanner.close();
+            System.out.println("¿Quieres jugar de nuevo? (Ingresa 0 para salir)");
+            try {
+                int respuesta = scanner.nextInt();
+                if (respuesta == 0) {
+                    jugando = false;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Ingresa una opción válida");
+                scanner.next();
+            }
         }
+
+        scanner.close();
     }
+}
+
+
 
--- src/edu/natalia/navarro/reto6/ui/CLI.java
+++ src/edu/natalia/navarro/reto6/ui/CLI.java
@@ -2,15 +2,17 @@ package edu.natalia.navarro.reto6.ui;
 import edu.natalia.navarro.reto6.Process.AdivinaLaPalabra;
 
 import java.util.Scanner;
+
 /**
  *  Clase que representa la interfaz de usuario del juego de adivinar palabras
  */
 public class CLI {
-    /**
-     * Método principal que se encarga de la ejecución del juego
-     * @param args Argumentos de la línea de comandos (no se utilizan en este caso)
-     */
-    public static void main(){
+    /*
+Método principal que se encarga de la ejecución del juego
+
+@param args Argumentos de la línea de comandos (no se utilizan en este caso)
+*/
+    public static void main() {
         Scanner scanner = new Scanner(System.in);
         int opcion = 0;
 
@@ -21,16 +23,24 @@ public class CLI {
             System.out.println("2. Intermedio");
             System.out.println("3. Difícil");
             System.out.println("0. Salir");
-            opcion = scanner.nextInt();
 
-            AdivinaLaPalabra juego = new AdivinaLaPalabra(opcion);
+            try {
+                opcion = scanner.nextInt();
 
-            if (opcion != 0) {
+                if (opcion == 0) {
+                    break;
+                } else if (opcion < 1 || opcion > 3) {
+                    System.out.println("Opción inválida");
+                    continue;
+                }
+
+                AdivinaLaPalabra juego = new AdivinaLaPalabra(opcion);
                 System.out.println("¡Comencemos a jugar!");
                 juego.jugar();
-                break;
+            } catch (Exception e) {
+                System.out.println("Opción inválida");
+                scanner.next();
             }
-
         } while (opcion != 0);
 
         System.out.println("¡Gracias por jugar!");
